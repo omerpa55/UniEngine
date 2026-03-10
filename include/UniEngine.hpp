f10ac91bebc164a287ae2c4c4ce72bc1dd7943af
Ömer PALA <palaomer100@gmail.com>
2026-03-10 09:48:44 +0300

    Added Windowing System

--- include/UniEngine.hpp
+++ include/UniEngine.hpp
@@ -2,9 +2,9 @@
 
 Copyright 2026 omerpa55
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation 
-files (the “Software”), to deal in the Software without restriction, 
-including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
+files (the “Software”), to deal in the Software without restriction,
+including without limitation the rights to use, copy, modify, merge, publish, distribute,
 sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so,
 subject to the following conditions:
 
@@ -21,10 +21,13 @@ DEALINGS IN THE SOFTWARE.
 #ifndef UNIENGINE_HPP
 #define UNIENGINE_HPP
 
-#include <vector>
 #if !defined (__cplusplus)
     #error This library is for C++
 #endif
+
+#include <vector>
+#include <string>
+#include <GLFW/glfw3.h>
 
 namespace Loc {
     struct Loc3D {
@@ -48,7 +51,7 @@ namespace Entity {
         MESH_2D = 1
     };
 
-    struct Entity3D {
+    struct RegistryEntity3D {
         int id;
         EntityType3D type;
         int parentId = -1;
@@ -56,23 +59,76 @@ namespace Entity {
 
         inline bool isRoot() const { return parentId == -1; };
     };
+
+
 }
 
 namespace Registry {
-    using Registry3D = std::vector<Entity::Entity3D>;
+    using Registry3D = std::vector<Entity::RegistryEntity3D>;
 
     inline Registry3D& getRegistry3D() {
         static Registry3D registry;
         return registry;
     }
 
-    inline Entity::Entity3D& createEntity3D(Entity::EntityType3D type, Loc::Loc3D loc) {
+    inline Entity::RegistryEntity3D& createRegistryEntity3D(Entity::EntityType3D type, Loc::Loc3D loc) {
         Registry3D& reg = getRegistry3D();
         int newID = static_cast<int>(reg.size());
 
         reg.push_back({ newID, type, -1, loc });
 
         return reg.back();
+    }
+
+    inline Entity::RegistryEntity3D& getRegisteryEntity3D(int id) {
+        return getRegistry3D()[id];
+    }
+}
+
+namespace Entity {
+    class Entity3D {
+    private:
+        int id;
+    public:
+        Entity3D(int id) : id(id) {};
+
+        inline void setLocation(float X, float Y, float Z) {
+            Entity::RegistryEntity3D& realEntity = Registry::getRegisteryEntity3D(id);
+            realEntity.loc.X = X;
+            realEntity.loc.Y = Y;
+            realEntity.loc.Z = Z;
+        }
+
+        inline void setParent(Entity3D parent) {
+            Entity::RegistryEntity3D& me = Registry::getRegisteryEntity3D(id);
+            me.parentId = parent.getID();
+        }
+
+        inline Loc::Loc3D getWorldLocation() const {
+            Entity::RegistryEntity3D me = Registry::getRegisteryEntity3D(id);
+            Entity::RegistryEntity3D myParent = Registry::getRegisteryEntity3D(me.parentId);
+            return { me.loc.X + myParent.loc.X, me.loc.Y + myParent.loc.Y, me.loc.Z + myParent.loc.Z };
+        }
+
+        inline Loc::Loc3D getLoc() const {
+            return Registry::getRegisteryEntity3D(id).loc;
+        }
+
+        inline int getID() const { return id; };
+    };
+}
+
+namespace Game {
+    struct WindowProperties {
+        int w;
+        int h;
+        std::string title;
+    };
+
+    typedef GLFWwindow* Window;
+
+    Window createWindow(WindowProperties* properties) {
+        return (Window) glfwCreateWindow(properties->w, properties->h, properties->title.c_str(), nullptr, nullptr);
     }
 }
 
--- uniengine.cpp
+++ uniengine.cpp
@@ -1,10 +1,6 @@
+#include "GLFW/glfw3.h"
 #include <UniEngine.hpp>
-#include <iostream>
-#include <ostream>
 
 int main() {
-    Entity::Entity3D player = Registry::createEntity3D(Entity::PLAYER_3D, { 0.0f, 0.0f, 0.0f });
-
-    std::cout << "PLAYER ID -> " << player.id << std::endl;
-    std::cout << "PLAYER PARENT -> " << player.parentId << std::endl;
+    
 }
